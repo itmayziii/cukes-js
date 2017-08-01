5bf57fdaeb365e27a85f151956de03809763d558
itmayziii <rangertmay@gmail.com>
2017-07-31 23:43:42 -0400

    Updated before|after all with comments

--- src/cucumber/hooks/after-all.ts
+++ src/cucumber/hooks/after-all.ts
@@ -2,7 +2,7 @@ import cucumber = require('cucumber');
 import { HookCode } from "cucumber";
 
 export function AfterAll(hookCode: HookCode) {
-    cucumber.defineSupportCode(function ({AfterAll}) { // TODO add to @types/cucumber
+    cucumber.defineSupportCode(function ({AfterAll}) { // TODO waiting for feature to be published - https://github.com/cucumber/cucumber-js/pull/878
         AfterAll(hookCode);
     });
 }
--- src/cucumber/hooks/before-all.ts
+++ src/cucumber/hooks/before-all.ts
@@ -2,7 +2,7 @@ import cucumber = require('cucumber');
 import { HookCode } from "cucumber";
 
 export function BeforeAll(hookCode: HookCode) {
-    cucumber.defineSupportCode(function ({BeforeAll}) { // TODO add to @types/cucumber
+    cucumber.defineSupportCode(function ({BeforeAll}) { // TODO waiting for feature to be published - https://github.com/cucumber/cucumber-js/pull/878
         BeforeAll(hookCode);
     });
 }
