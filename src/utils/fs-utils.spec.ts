2cf8f5f69b709a20b95f2420bd34d2cdcb1602d9
= <=>
2017-08-04 14:01:12 -0400

    Now have all tests working for fs-utils

--- src/utils/fs-utils.spec.ts
+++ src/utils/fs-utils.spec.ts
@@ -1,6 +1,7 @@
 import * as fsUtils from './fs-utils';
 import { PathLike } from "fs";
 import * as path from "path";
+import * as fs from "fs";
 
 describe('fs-utils', () => {
 
@@ -8,14 +9,13 @@ describe('fs-utils', () => {
 
         it('should parse a json file into a JSON object', () => {
             const packageJsonPath: PathLike = path.resolve(__dirname, '../../package.json');
-            console.log(packageJsonPath);
             fsUtils.readJsonFile(packageJsonPath)
                 .then((packageJson) => {
                     expect(packageJson.name).toBe('cukes-js');
                 })
                 .catch(() => {
                     fail('the function should not throw an error');
-                })
+                });
         });
 
         it('should reject a promise with an error if the file does not exist', () => {
@@ -42,11 +42,16 @@ describe('fs-utils', () => {
     });
 
     describe('listFeatureFiles()', () => {
+        const featureFileLocation = path.resolve(__dirname, 'features');
+
+        beforeAll(() => {
+            createFeatureFiles(featureFileLocation);
+        });
 
         it('should return only files ending with .feature given a directory', () => {
-            fsUtils.listFeatureFiles(__dirname)
+            fsUtils.listFeatureFiles(featureFileLocation)
                 .then((files) => {
-                    expect(files).toEqual(['feature-file.feature']);
+                    expect(files).toEqual(['first.feature', 'second.feature', 'third.feature']);
                 })
                 .catch(() => {
                     fail('the function should not be throwing an error');
@@ -65,4 +70,70 @@ describe('fs-utils', () => {
 
     });
 
+    describe('deleteFile()', () => {
+        const featureFileLocation = path.resolve(__dirname, 'features');
+
+        beforeAll(() => {
+            createFeatureFiles(featureFileLocation)
+        });
+
+        it('should fulfill a promise if the file was successfully deleted', () => {
+            const existingFile = path.resolve(featureFileLocation, 'first.feature');
+            fsUtils.deleteFile(existingFile)
+                .then(() => {
+                    const featureFiles = fs.readdirSync(featureFileLocation);
+                    expect(featureFiles.length).toBe(2);
+                })
+                .catch(() => {
+                    fail('the function should not reject the promise on successful deletion of a file');
+                });
+        });
+
+        it('should reject a promise if the file was not deleted', () => {
+            fsUtils.deleteFile('this-file-will-never-exist-hopefully.js')
+                .then(() => {
+                    fail('the function did not reject the promise if the file was not deleted');
+                })
+                .catch((error) => {
+                    expect(true).toBe(true);
+                });
+        });
+
+    });
+
+
+    describe('clearOutputDirectory', () => {
+        const featureFileLocation = path.resolve(__dirname, 'features');
+
+        beforeAll(() => {
+            createFeatureFiles(featureFileLocation);
+        });
+
+        it('should fulfill a promise if the files were deleted successfully', () => {
+            fsUtils.clearOutputDirectory(featureFileLocation)
+                .then(() => {
+                    const featureFiles = fs.readdirSync(featureFileLocation);
+                    expect(featureFiles.length).toBe(0);
+                })
+                .catch(() => {
+                    fail('the function should not reject the promise on successful deletion of files');
+                })
+        });
+
+    });
+
 });
+
+function createFeatureFiles(featureFilePath: PathLike): void {
+    const featureFiles = ['first.feature', 'second.feature', 'third.feature'];
+
+    if (!fs.existsSync(featureFilePath)) {
+        fs.mkdirSync(featureFilePath);
+    }
+
+
+    featureFiles.forEach((fileName) => {
+        const fileLocation = path.resolve(featureFilePath, fileName);
+        fs.writeFileSync(fileLocation, fileName)
+    });
+}
